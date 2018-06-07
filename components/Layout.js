50833d46e2658c6fb2f011f5f343e2620e7c0d50
Juan David Castro <me@juandc.co>
2018-06-07 15:49:52 -0500

    :iphone: Landscape support for layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,9 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, ScrollView, Text } from "react-native";
 import { colors } from "../styles/common";
 
-export default props => <View style={styles.layout} {...props} />;
+export default props => <ScrollView style={styles.layout} {...props} />;
 
 const styles = StyleSheet.create({
-  layout: {
-    backgroundColor: colors.black,
-    flex: 1,
-    padding: 16
-  }
+  layout: { backgroundColor: colors.black, flex: 1, padding: 16 }
 });
